42de4d7bda72650c6fd76dd3005c0e6b4832d144
wandieinnocents <wandieinnocent2@gmail.com>
2023-01-26 12:24:30 +0300

    Modify livewire component

--- app/Http/Livewire/Admin/Category/Index.php
+++ app/Http/Livewire/Admin/Category/Index.php
@@ -3,11 +3,18 @@
 namespace App\Http\Livewire\Admin\Category;
 
 use Livewire\Component;
+use Livewire\withPagination;
+use App\Models\Category;
 
 class Index extends Component
 {
+    use withPagination;
+    protected $paginationTheme = 'bootstrap';
+
     public function render()
     {
-        return view('livewire.admin.category.index');
+        $categories = Category:orderBy('id','DESC')->paginate(10);
+
+        return view('livewire.admin.category.index' , ['categories' => $categories]);
     }
 }
